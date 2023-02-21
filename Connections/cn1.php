2cf5838f9c18bfe861fce9ed05e980717ebed231
katsuragy07 <71047899+katsuragy07@users.noreply.github.com>
2023-02-21 00:14:40 -0500

    Update cn1.php

--- Connections/cn1.php
+++ Connections/cn1.php
@@ -5,7 +5,7 @@
 $hostname_cn1 = "localhost";
 $database_cn1 = "uncp_tramite";
 $username_cn1 = "root";
-$password_cn1 = "ae9a56ed87";
+$password_cn1 = "";
 $cn1 = mysql_pconnect($hostname_cn1, $username_cn1, $password_cn1); 
 mysql_select_db($database_cn1,$cn1);
 
